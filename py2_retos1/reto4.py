2efba2e0403f3ab4b651bbde6cc3e1eb6e762ebd
FabianSuarezBot <>
2021-04-22 19:47:46 +0000

    Reto 4 SM

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,9 @@
+a = int(input('Ingrese un numero:'))
+b = int(input('Ingrese otro numero:'))
+
+if (a < b):
+  print(f'{a} es menor que {b}')
+elif (a > b):
+  print(f'{a} es mayor que {b}')
+elif (a == b):
+  print(f'{a} y {b} son iguales')
