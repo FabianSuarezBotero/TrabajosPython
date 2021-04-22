f4631453ccf2f9de6d2d48958e0cfdbc807d211f
FabianSuarezBot <>
2021-04-22 20:22:26 +0000

    Reto 14

---
+++ py2_retos1/reto14.py
@@ -0,0 +1,19 @@
+
+#################
+def cuadrado (num):
+  cuad = num ** 2
+  return cuad
+
+def progfin ():
+  a = 'El programa ha finalizado'
+  return (a)
+
+#################
+
+a = range (-10,11)
+for i in a:
+  print (cuadrado (i))
+
+
+  
+print (progfin())
