c1215637a9db52305adecae3fdda7e0ec3dcbdcc
FabianSuarezBot <>
2021-04-28 03:12:44 +0000

    Ejercicio sumatoria

---
+++ py3_ciclos/sumatoria.py
@@ -0,0 +1,16 @@
+A = [4, 6, 8]
+B = [2, 2, 2]
+C = [1, 2, 3]
+
+def sumatoria (A, B, C):
+  n = len (A)
+  acumulador = 0
+  for i in range (n):
+    d = A[i]*B[i]
+    e = d + C[i]
+    acumulador = acumulador + e
+  resultado = acumulador + n**2
+  return resultado
+
+
+sumatoria (A, B, C)
