a897f79a9ca52cd910ab7bab81ed07f174dafbb7
FabianSuarezBot <81332748+FabianSuarezBotero@users.noreply.github.com>
2021-08-19 19:53:59 +0000

    reto 11 ok

---
+++ py6_retos2/reto11.py
@@ -0,0 +1,6 @@
+lista1 = list(range(11))
+lista2 = list(range(11,21))
+
+print (f'La lista 1 es: {lista1}')
+print (f'La lista 2 es: {lista2}')
+print (f'La union de las dos listas es: {lista1 + lista2}')
